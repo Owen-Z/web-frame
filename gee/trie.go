f885115b43617b62a10ae07309e0a81e3bfc801f
Owen-Z <1696685042@qq.com>
2022-05-17 18:51:15 +0800

    构建了前缀树用于存储动态路由，提供了查询和插入的操作

---
+++ gee/trie.go
@@ -0,0 +1,68 @@
+package gee
+
+import "strings"
+
+type node struct {
+	pattern  string  // wait for matching route	  e.g. /p/:lang
+	part     string  // part of route  e.g. :lang
+	children []*node // child node	e.g. [doc, tutorial, intro]
+	isWild   bool    // the node needed to match vaguely	e.g. part contain : or * 时为true
+}
+
+// find the first node match the part
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+func (n *node) matchChildren(part string) []*node {
+	// slice need to check out the capacity is enough or not
+	// if the capacity is not enough, the cap should be increased
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nodes
+}
+
+func (n *node) insert(pattern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = pattern
+		return
+	}
+
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
+		n.children = append(n.children, child)
+	}
+	child.insert(pattern, parts, height+1)
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
+}
